45d624a6d1f9ef740c311cb871f4b30b755b8e6f
Maya <mayarin@users.noreply.github.com>
2020-10-29 13:11:58 +0900

    first commit

---
+++ src/config/config.js
@@ -0,0 +1,24 @@
+
+module.exports = {
+  development: {
+    username: "root",
+    password: "password",
+    database: "todo",
+    host: "mysql",
+    dialect: 'mysql',
+  },
+  test: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    host: process.env.MYSQL_SERVER,
+    dialect: 'mysql',
+  },
+  production: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    host: process.env.MYSQL_SERVER,
+    dialect: 'mysql',
+  },
+};
