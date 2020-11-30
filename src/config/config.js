4f7a275b52cc9358b9b49397c7cef3db78fe4f06
Maya <mayarin@users.noreply.github.com>
2020-11-30 19:24:02 +0900

    mailhog->gmail

--- src/config/config.js
+++ src/config/config.js
@@ -1,24 +1,52 @@
+// データベース名に DB_NAME の値を使用する
+let database = process.env.DB_NAME;
 
+// データベース接続の際のユーザに DB_USER の値を使用する
+let username = process.env.DB_USER;
+
+// データベース接続の際のユーザパスワードに DB_PASSWORD の値を使用する
+let password = process.env.DB_PASSWORD;
+
+// データベース接続の際のホストに DB_HOST の値を使用する (デフォは localhost)
+let host = process.env.DB_HOST || 'localhost';
+
+// データベース接続の際のポートに DB_PORT の値を使用する (デフォは 5432)
+let port = process.env.DB_PORT || '5432';
+
+// dialect の 'postgres' は使用するデータベースにより mysql とかにする
+// 使用可能なデータベース一覧 (https://github.com/sequelize/sequelize#installation)
 module.exports = {
   development: {
+    database: "todo",
     username: "root",
     password: "password",
-    database: "todo",
-    host: "mysql",
-    dialect: 'mysql',
+    host: "127.0.0.1",
+    port: "3306",
+    dialect: 'postgres',
+    define: {
+      underscored: true
+    },
   },
   test: {
+    database: "todo",
     username: "user",
     password: "dvQXL55e",
-    database: "todo",
     host: "54.150.104.149",
-    dialect: 'mysql',
+    port: "3306",
+    dialect: 'postgres',
+    define: {
+      underscored: true
+    },
   },
   production: {
+    database: "todo",
     username: "user",
     password: "dvQXL55e",
-    database: "todo",
     host: "54.150.104.149",
-    dialect: 'mysql',
+    port: "3306",
+    dialect: 'postgres',
+    define: {
+      underscored: true
+    },
   },
-};
+}
