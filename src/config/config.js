8d0bff06e4113fe5ced797155cfed61088c302e7
Maya <mayarin@users.noreply.github.com>
2020-11-30 18:07:02 +0900

    mailhog->gmail

--- src/config/config.js
+++ src/config/config.js
@@ -15,10 +15,10 @@ module.exports = {
     dialect: 'mysql',
   },
   production: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    host: process.env.MYSQL_SERVER,
+    username: "user",
+    password: "dvQXL55e",
+    database: "todo",
+    host: "54.150.104.149",
     dialect: 'mysql',
   },
 };
