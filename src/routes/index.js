09333dcd72c2a8905d41cea821bf40371c2e5d07
Maya <mayarin@users.noreply.github.com>
2020-12-03 16:49:51 +0900

    mailhog->gmail

--- src/routes/index.js
+++ src/routes/index.js
@@ -131,7 +131,7 @@ router.post('/signup', async function(req, res) {
       const now = new Date();
       console.log('L132 '+now.getHours());
       const expiration = now.setHours(now.getHours() + 1); // 1時間だけ有効
-      console.log('L134 '+now.getHours());
+      console.log('L134 '+now.getHours(), now.getHours() + 1);
       console.log('L135 '+expiration);
       let verificationUrl = req.get('origin') +'/verify/'+ user.id +'/'+ hash +'?expires='+ expiration;
       const signature = crypto.createHmac('sha256', appKey)
